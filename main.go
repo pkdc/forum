7155c75a64dae8709110ef6c02c4362f1c7d2ac1
pkdch <pau.darco371@gmail.com>
2022-05-20 14:09:26 +0100

    for hosting

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"forum/forum"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func main() {
+	forum.InitDB()
+	// forum.ClearUsers()
+	// forum.ClearPosts()
+	// forum.ClearComments()
+	exec.Command("xdg-open", "http://localhost:8080/").Start()
+
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", forum.HomeHandler)
+	http.HandleFunc("/login", forum.LoginHandler)
+	http.HandleFunc("/register", forum.RegisterHandler)
+	http.HandleFunc("/logout", forum.LogoutHandler)
+	http.HandleFunc("/postpage", forum.PostPageHandler)
+	// http.HandleFunc("/delete", forum.DeleteHandler)
+	fmt.Println("Starting server at port 80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
